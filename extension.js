9c1df86b24af87d095c4ca991a1fc5dbcc9c113d
Daniel García Moreno <dani@danigm.net>
2021-08-31 12:38:01 +0200

    Move imports to the top

--- extension.js
+++ extension.js
@@ -1,24 +1,26 @@
 const isOverviewWindow = imports.ui.workspace.Workspace.prototype._isOverviewWindow;
-const getWindows = imports.ui.altTab.getWindows;
+const { Workspace } = imports.ui.workspace;
+const { altTab } = imports.ui;
+const { getWindows } = altTab;
 
 function init() {
 }
 
 function enable() {
-  imports.ui.workspace.Workspace.prototype._isOverviewWindow = (win) => {
+  Workspace.prototype._isOverviewWindow = (win) => {
     const show = isOverviewWindow(win);
     let meta = win;
     if (win.get_meta_window)
       meta = win.get_meta_window()
     return show && !meta.minimized;
   };
-  imports.ui.altTab.getWindows = (workspace) => {
+  altTab.getWindows = (workspace) => {
     const windows = getWindows(workspace);
     return windows.filter((w, i, a) => !w.minimized);
   };
 }
 
 function disable() {
-  imports.ui.workspace.Workspace.prototype._isOverviewWindow = isOverviewWindow;
-  imports.ui.altTab.getWindows = getWindows;
+  Workspace.prototype._isOverviewWindow = isOverviewWindow;
+  altTab.getWindows = getWindows;
 }
